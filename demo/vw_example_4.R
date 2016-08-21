bbefb1a5f10ff9eff9bf35bfe4e3f68c5b50e593
Dirk Eddelbuettel <edd@debian.org>
2016-08-21 14:29:14 -0500

    now plots (up to) three ROC lines at end

--- demo/vw_example_4.R
+++ demo/vw_example_4.R
@@ -1,5 +1,6 @@
 
 library(rvw)
+library(ggplot2)
 
 data("etitanic", package="earth")
 dt <- etitanic
@@ -7,7 +8,7 @@ dt <- etitanic
 
 
 set.seed(123)                           # arbitrary but fixed seed
-ind_train <- sample(1:nrow(dt), 0.75*nrow(dt))  # separate train and validation data
+ind_train <- sample(1:nrow(dt), 0.8*nrow(dt))  # separate train and validation data
 dt_train <- dt[ind_train,]
 dt_val <- dt[-ind_train,]
 
@@ -24,7 +25,7 @@ resvw <- vw(training_data = dt_train,
             passes = 10,
             verbose = TRUE)
 resvw[["data"]][, actual:=as.factor(dt_val$survived)]
-
+dd <- resvw[["data"]]
 setwd(cwd)                              # go back
 
 if (requireNamespace("caret", quietly=TRUE)) {
@@ -33,22 +34,33 @@ if (requireNamespace("caret", quietly=TRUE)) {
 
 rvw:::plotDensity(resvw[["data"]])   ## TODO: plot method of a class vw
 
+rocvw <- roc(dd[,actual], dd[, predicted])
+plot(rocvw)
+
 
 ## glm
 resglm <- glm(survived ~ pclass + sex + age + sibsp + parch, family = binomial(logit), data = dt_train)
-predglm <- predict(resglm, dt_val)
+predglm <- predict(resglm, dt_val, type="response")
 if (requireNamespace("caret", quietly=TRUE)) {
     caret::confusionMatrix(ifelse(predglm >= 0.5, 1, 0), dt_val$survived)
 }
+rocglm <- roc(dd[,actual], predglm)
+plot(rocglm, col="orange", add=TRUE)
 
 
 ## rf
 if (requireNamespace("randomForest", quietly=TRUE)) {
     library(randomForest)
     resrf <- randomForest(as.factor(survived) ~ pclass + sex + age + sibsp + parch, data=dt_train,
-                          ntree=5000, importance=TRUE)
+                          ntree=5000, importance=TRUE, keep.forest=TRUE)
     predrf <- predict(resrf, dt_val)
+    predrfprob <- predict(resrf, dt_val, type="prob")
     if (requireNamespace("caret", quietly=TRUE)) {
-        caret::confusionMatrix(predrf, dt_val$survived)
+        caret::confusionMatrix(as.integer(as.character(predrf)), dt_val$survived)
     }
+    rocrf <- roc(dd[,actual], predrfprob[,1])
+    plot(rocrf, col="blue", add=TRUE)
+
 }
+
+
